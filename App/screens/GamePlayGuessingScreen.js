61c2ed7b0da6f839b96b1e5985d440fe87b83917
github username <sean.ottomanelli@gmail.com>
2021-10-18 11:43:08 -0400

    saved one file

--- App/screens/GamePlayGuessingScreen.js
+++ App/screens/GamePlayGuessingScreen.js
@@ -2,11 +2,16 @@ import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import styles from "../constants/styles";
 import Player from "../components/Player";
+import { useSelector } from "react-redux";
 
 export default function GamePlayGuessingScreen({navigation}) {
+
+    
+
     return(
         <View style={styles.container}>
             <Text style={styles.titleText}>Select Winner</Text>
+
             <Player player = {"Player 1"}/>
             <Player player = {"Player 2"}/>
             <Player player = {"Player 3"}/>
