143578c2b8efa61cb469895fc969268ed49f85a7
jennafritz <jennafritz24@gmail.com>
2021-10-25 16:33:22 -0400

    Implemented point-saving functionality. Cleared up some bugs about navigation stack and clearing state. Added song fetching for all turns, not just refreshing.

--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -1,14 +1,26 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
-import React from "react";
+import React, {useEffect} from "react";
 import colors from "../constants/colors";
 import styles from "../constants/styles";
+import { useDispatch } from "react-redux";
+import { clearSongGroups } from "../config/Reducers/SongsReducer";
+import { clearDecades } from "../config/Reducers/DecadesReducer";
+import { clearUserGameState } from "../config/Reducers/UserGamesReducer";
 
 export default function HomeScreen({navigation}) {
+
+    const dispatch = useDispatch()
+
     return(
         <View style={styles.container}>
             <Text style={styles.titleText}>Hummingbird</Text>
             <Image source={require('../assets/hummingbirdcopy.png')} style={styles.logo}/>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.push("PlayerSetup")}>
+            <TouchableOpacity style={styles.button} onPress={() => {
+                navigation.push("PlayerSetup")
+                dispatch(clearDecades())
+                dispatch(clearSongGroups())
+                dispatch(clearUserGameState())
+            }}>
                 <Text style={styles.buttonText}>Start Game</Text>
             </TouchableOpacity>
         </View>
--- App/screens/LeaderboardScreen.js
+++ App/screens/LeaderboardScreen.js
@@ -1,11 +1,12 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 import React from 'react'
 import styles from '../constants/styles'
 import LeaderboardItem from '../components/LeaderboardItem'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { clearDecades } from '../config/Reducers/DecadesReducer'
 import { clearSongGroups } from '../config/Reducers/SongsReducer'
-
+import { clearUserGameState } from '../config/Reducers/UserGamesReducer'
+import { StackActions } from '@react-navigation/routers'
 
 const decadeButtonContainer ={
     ...styles.container,
@@ -16,21 +17,22 @@ export default function LeaderboardScreen({ navigation }) {
 
     const dispatch = useDispatch()
 
+    const leaders = useSelector(state => state.players.topPlayers)
+
     return (
         <View style={decadeButtonContainer}>
             <Text style = {styles.titleText}>Leaderboard</Text>
-            <LeaderboardItem playerInfo = {{name: "Sean", points: 20}}/>
-            <LeaderboardItem playerInfo = {{name: "Sean", points: 20}}/>
-            <LeaderboardItem playerInfo = {{name: "Sean", points: 20}}/>
-            <LeaderboardItem playerInfo = {{name: "Sean", points: 20}}/>
-            <LeaderboardItem playerInfo = {{name: "Sean", points: 20}}/>
-            <LeaderboardItem playerInfo = {{name: "Sean", points: 20}}/>
-            <LeaderboardItem playerInfo = {{name: "Sean", points: 20}}/>
-            <LeaderboardItem playerInfo = {{name: "Sean", points: 20}}/>
+            <FlatList 
+                data={leaders} 
+                keyExtractor={leader => leader.id.toString()}
+                renderItem={({item}) => <LeaderboardItem playerInfo={item}/>}
+            />
             <TouchableOpacity style={styles.button} onPress={() => {
-                navigation.push("Home")
-                dispatch(clearDecades())
-                dispatch(clearSongGroups())}}>
+                navigation.dispatch(StackActions.popToTop());
+                // dispatch(clearDecades())
+                // dispatch(clearSongGroups())
+                // dispatch(clearUserGameState())
+                }}>
                 <Text style={styles.buttonText}>
                     Start New Game
                 </Text>
